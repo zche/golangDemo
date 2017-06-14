e4e614158ceaa27f3799e59ebdc864cbd8c5147a
che <che@localhost.localdomain>
2017-06-14 11:08:12 +0800

    为类型定义方法

---
+++ TypeSystem/int.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+//Integer 表示一个整形
+type Integer int
+
+//Less 比较大小
+func (a Integer) Less(b Integer) bool {
+	return a < b
+}
+
+func main() {
+	var a Integer = 1
+	if a.Less(2) {
+		fmt.Println(a, "Less 2")
+	}
+
+}

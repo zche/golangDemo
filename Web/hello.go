ef3558840f40253b1bb32e53ec479c6d90a046e4
che <che@localhost.localdomain>
2017-06-19 14:06:32 +0800

    modify

--- Web/hello.go
+++ Web/hello.go
@@ -15,7 +15,7 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 }
 func main() {
 	http.HandleFunc("/", helloHandler)
-	err := http.ListenAndServe(":8080", nil)
+	err := http.ListenAndServe(":8888", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe:", err.Error())
 	}
